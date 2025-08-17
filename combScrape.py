fe7a7e98b71ca825acf6476a269a156197fa4df7
NorthernL1ghts <137669199+AthithIttiphakorn@users.noreply.github.com>
2025-08-17 09:10:05 +0700

    Update combScrape.py
    try and accept to evade pages given to bots

--- combScrape.py
+++ combScrape.py
@@ -12,7 +12,7 @@ def conv_timeToSeconds(time_str: str) -> str:
     except:
         return str(time_str)
 
-for i in range(1):
+for i in range(30):
     conn = http.client.HTTPSConnection("www.swimrankings.net")
     conn.request("GET", f"/index.php?page=athleteDetail&athleteId={athleteID}&styleId=13")
     response = conn.getresponse()
@@ -26,32 +26,35 @@ for i in range(1):
 
     timesList = []
     datesList = []
+    try:
+        # Get the first athleteRanking table (Long Course)
+        two_columns = soup.find("table", class_="twoColumns")
+        course_sections = two_columns.find_all("table", class_="athleteRanking")
+        #print(course_sections)
+        if course_sections:
+            lc_section = course_sections[0]  # Long Course always comes first
+            #print(lc_section)
 
-    # Get the first athleteRanking table (Long Course)
-    two_columns = soup.find("table", class_="twoColumns")
-    course_sections = two_columns.find_all("table", class_="athleteRanking")
-    #print(course_sections)
-    if course_sections:
-        lc_section = course_sections[0]  # Long Course always comes first
-        #print(lc_section)
+            times = lc_section.find_all("a", class_="time")
+            dates = lc_section.find_all("td", class_="date")
 
-        times = lc_section.find_all("a", class_="time")
-        dates = lc_section.find_all("td", class_="date")
+            for currentTime in times:
+                timesList.append(conv_timeToSeconds(currentTime.get_text()))
 
-        for currentTime in times:
-            timesList.append(conv_timeToSeconds(currentTime.get_text()))
+            for day in dates:
+                datesList.append(day.get_text())
 
-        for day in dates:
-            datesList.append(day.get_text())
+        # Write results if we actually found LC data
+        if timesList:
+            with open('times.csv', 'a') as f:
+                f.write(f"{athleteID}," + ','.join(timesList) + '\n')
 
-    # Write results if we actually found LC data
-    if timesList:
-        with open('times.csv', 'a') as f:
-            f.write(f"{athleteID}," + ','.join(timesList) + '\n')
+        if datesList:
+            with open('dates.csv', 'a') as f:
+                f.write(f"{athleteID}," + ','.join(datesList) + '\n')
 
-    if datesList:
-        with open('dates.csv', 'a') as f:
-            f.write(f"{athleteID}," + ','.join(datesList) + '\n')
+    except:
+        time.sleep(1)
 
     athleteID += 1
-    time.sleep(1)
+    time.sleep(2.3)
